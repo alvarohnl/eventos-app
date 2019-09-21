0b6441a81305d6e234fe421be2e3c379e35d2091
Alvaro Nogueira <nogueira.alvaro@gmail.com>
2019-09-21 19:03:46 -0300

    Criando tela login

--- app/src/main/java/alvarohnl/eventos/app/login/view/LoginActivity.java
+++ app/src/main/java/alvarohnl/eventos/app/login/view/LoginActivity.java
@@ -1,14 +1,16 @@
-package alvarohnl.eventos.app;
+package alvarohnl.eventos.app.login.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import alvarohnl.eventos.app.R;
+
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
     }
 }
