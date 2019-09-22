ff95a3bd79c67921adcb1fd1a923c34282d4a48c
Alvaro Nogueira <nogueira.alvaro@gmail.com>
2019-09-21 21:43:27 -0300

    Adicionando timer logout

--- app/src/main/java/alvarohnl/eventos/app/login/view/LoginActivity.java
+++ app/src/main/java/alvarohnl/eventos/app/login/view/LoginActivity.java
@@ -8,6 +8,10 @@ import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
+import alvarohnl.eventos.app.EventosApplication;
 import alvarohnl.eventos.app.R;
 import alvarohnl.eventos.app.eventos.view.EventosActivity;
 import alvarohnl.eventos.app.login.data.model.UsuarioToken;
@@ -47,6 +51,11 @@ public class LoginActivity extends AppCompatActivity implements LoginContract.Vi
     @Override
     public void loginSucesso(UsuarioToken usuarioToken) {
 
+        Timer timer = new Timer();
+        LogOutTimerTask logoutTimeTask = new LogOutTimerTask();
+
+        timer.schedule(logoutTimeTask, Long.valueOf(usuarioToken.getTempoExpirar()) * 1000);
+
         Intent intent = new Intent(this, EventosActivity.class);
         startActivity(intent);
 
@@ -57,6 +66,20 @@ public class LoginActivity extends AppCompatActivity implements LoginContract.Vi
         editEmail = findViewById(R.id.editUsuario);
         editSenha = findViewById(R.id.editSenha);
         botaoAcessar = findViewById(R.id.acessarButton);
+
+    }
+
+    private class LogOutTimerTask extends TimerTask {
+
+        @Override
+        public void run() {
+            
+            Intent i = new Intent(EventosApplication.getInstance().getApplicationContext(), LoginActivity.class);
+            i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(i);
+            finish();
+
+        }
 
     }
 
